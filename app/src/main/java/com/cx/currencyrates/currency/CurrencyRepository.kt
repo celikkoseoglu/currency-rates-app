ae0c9fb13966334a46c905e6c5a3606573a325a4
Celik Koseoglu <celikkoseoglu@yahoo.com>
2019-07-28 14:54:01 +0100

    added top padding to recyclerview

--- app/src/main/java/com/cx/currencyrates/currency/CurrencyRepository.kt
+++ app/src/main/java/com/cx/currencyrates/currency/CurrencyRepository.kt
@@ -7,7 +7,6 @@ import com.cx.currencyrates.currency.model.CurrencyMapper
 import io.reactivex.Single
 
 internal class CurrencyRepository(private val currencyService: CurrencyService, private val currencyMapper: CurrencyMapper) {
-
     fun currencyRates(): Single<List<Currency>> {
         return currencyService.getExchangeRates("GBP").map { currencyMapper.map(it) }
     }
